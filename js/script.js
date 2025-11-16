54cf64d4b5cf260166919c98f73bd6b235930be2
Juan Cruz <juancbonelli@gmail.com>
2025-11-16 21:20:05 +0100

    últimos detalles de lógica y estilos

--- js/script.js
+++ js/script.js
@@ -3,6 +3,14 @@ console.log(peliculas);
 
 const generos = [28, 53, 12];
 
+// const generoSet = new Set();
+
+// peliculas.forEach((pelicula) => {
+//   pelicula.genre_ids.forEach((id) => generoSet.add(id));
+// });
+// const generos = Array.from(generoSet);
+// console.log(generos);
+
 generos.forEach((genero) => {
   const pelisFiltradas = peliculas.filter((pelicula) =>
     pelicula.genre_ids.includes(genero)
@@ -14,13 +22,13 @@ function poblar(pelisFiltradas, pelisContainer) {
   pelisFiltradas.forEach((pelicula) => {
     const titulo = pelicula.original_title;
     const imgPath = pelicula.backdrop_path;
-    const peliContainer = document.createElement("div");
+    const peliCard = document.createElement("div");
     const imgContainer = document.createElement("img");
     imgContainer.src = `https://image.tmdb.org/t/p/w500${imgPath}`;
-    pelisContainer.appendChild(imgContainer);
+    peliCard.appendChild(imgContainer);
     const titleContainer = document.createElement("h3");
     titleContainer.textContent = `${titulo}`;
-    pelisContainer.appendChild(titleContainer);
-    pelisContainer.appendChild(peliContainer);
+    peliCard.appendChild(titleContainer);
+    pelisContainer.appendChild(peliCard);
   });
 }
